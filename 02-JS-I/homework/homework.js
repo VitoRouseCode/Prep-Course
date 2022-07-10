af85eb9d9c9ece15e1702eb1b20266108756ff74
Victor Online <victorbellasartes@gmail.com>
2022-07-09 22:33:24 -0500

    segunda

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -32,7 +32,7 @@ function devolverString(str) {
   
 }
 
-function sumar(x, y) {
+function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
@@ -89,20 +89,29 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if(num<90){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(num>50){
+    return true;
+  }else{
+    return false;
+  }
   
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  console.log(x % y);
 }
 
 function esPar(num) {
